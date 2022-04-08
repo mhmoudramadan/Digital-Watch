67fb7c454e731ef911363ab1ded7eb4d92457908
mhmoudramadan <mhmoudramadan111@gmail.com>
2022-04-08 03:33:48 +0200

    1:0:0

---
+++ Debug/main.d
@@ -0,0 +1,21 @@
+main.o main.o: ../main.c ../LIB/LSTD_TYPES.h \
+  ../MCAL/MDIO/MDIO_Interface.h ../MCAL/MEXTI/MEXTI_Config.h \
+  ../MCAL/MEXTI/MEXTI_Interface.h ../MCAL/MGIE/MGIE_Interface.h \
+  ../MCAL/MTIMER1/MTIMER1_Interface.h ../HAL/HCLCD/HCLCD_Config.h \
+  ../HAL/HCLCD/HCLCD_Interface.h
+
+../LIB/LSTD_TYPES.h:
+
+../MCAL/MDIO/MDIO_Interface.h:
+
+../MCAL/MEXTI/MEXTI_Config.h:
+
+../MCAL/MEXTI/MEXTI_Interface.h:
+
+../MCAL/MGIE/MGIE_Interface.h:
+
+../MCAL/MTIMER1/MTIMER1_Interface.h:
+
+../HAL/HCLCD/HCLCD_Config.h:
+
+../HAL/HCLCD/HCLCD_Interface.h:
